61121763430e6d28dff833dbac743a8f83c2c1c7
Tim Sutton <tim@linfiniti.com>
2014-05-29 17:05:50 +0200

    Fix run script

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 
 docker.io run \
 	--name=tilestream \
-	--link osm-africa-postgis:osm-africa-postgis \
         -v /home/gisdata:/home/gisdata \
-        -v /home/timlinux/Documents/MapBox:/root/Documents/MapBox \
+        -v /home/timlinux/Documents/MapBox:/Documents/MapBox \
 	-p 8887:22 \
 	-p 8888:8888 \
 	-p 8889:8889 \
